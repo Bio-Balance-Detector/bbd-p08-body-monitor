509fad293859ffa7c188efb282cf4c324bfd655e
Andras Fuchs <andras.fuchs@gmail.com>
2023-06-22 20:42:19 +0200

    Apply Code Cleanup profile

--- Source/BBD.BodyMonitor.API/Controllers/FitbitController.cs
+++ Source/BBD.BodyMonitor.API/Controllers/FitbitController.cs
@@ -1,5 +1,4 @@
 using BBD.BodyMonitor.Configuration;
-using BBD.BodyMonitor.Controllers;
 using BBD.BodyMonitor.Services;
 using BBD.BodyMonitor.Sessions;
 using BBD.BodyMonitor.Sessions.Segments;
@@ -11,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System.Text.Json;
 
-namespace BBD.BodyMonitor.API.Controllers
+namespace BBD.BodyMonitor.Controllers
 {
     [ApiController]
     [Route("[controller]")]
@@ -20,8 +19,8 @@ namespace BBD.BodyMonitor.API.Controllers
         private readonly ILogger<SystemController> _logger;
         private readonly IConfiguration _config;
         private readonly ISessionManagerService _sessionManager;
-        private FitbitAppCredentials appCredentials = new FitbitAppCredentials();
-        private string redirectURL;
+        private readonly FitbitAppCredentials appCredentials = new();
+        private readonly string redirectURL;
 
         private static OAuth2AccessToken? currentAccessToken;
         private static string accessTokenFilename;
@@ -57,7 +56,7 @@ namespace BBD.BodyMonitor.API.Controllers
         [Route("authenticate")]
         public string Authenticate()
         {
-            var authenticator = new OAuth2Helper(appCredentials, redirectURL);
+            OAuth2Helper authenticator = new(appCredentials, redirectURL);
             string[] scopes = new string[] { "profile", "sleep", "heartrate", "respiratory_rate" };
 
             string authUrl = authenticator.GenerateAuthUrl(scopes, null);
@@ -69,17 +68,17 @@ namespace BBD.BodyMonitor.API.Controllers
         [Route("accesstoken")]
         public async void AccessToken(string code)
         {
-            if (String.IsNullOrEmpty(appCredentials.ClientSecret))
+            if (string.IsNullOrEmpty(appCredentials.ClientSecret))
             {
                 throw new Exception("Fitbit client secret is not defined. Set the 'BODYMONITOR__FITBIT__CLIENTSECRET' enviroment variable to the client secret that you got when you registered your app at https://dev.fitbit.com/apps/.");
             }
 
-            var authenticator = new OAuth2Helper(appCredentials, redirectURL);
+            OAuth2Helper authenticator = new(appCredentials, redirectURL);
 
-            FitbitController.currentAccessToken = await authenticator.ExchangeAuthCodeForAccessTokenAsync(code);
+            currentAccessToken = await authenticator.ExchangeAuthCodeForAccessTokenAsync(code);
 
             // save it for later use
-            var accessTokenJson = JsonSerializer.Serialize(currentAccessToken, new JsonSerializerOptions
+            string accessTokenJson = JsonSerializer.Serialize(currentAccessToken, new JsonSerializerOptions
             {
                 WriteIndented = true
             });
@@ -90,15 +89,15 @@ namespace BBD.BodyMonitor.API.Controllers
         [Route("getprofile/{encodedUserId}")]
         public UserProfile GetProfile(string encodedUserId)
         {
-            if (FitbitController.currentAccessToken == null)
+            if (currentAccessToken == null)
             {
                 throw new Exception("Fitbit access token is null. You must first call 'fitbit/authenticate' and follow the URL that is returned.");
             }
 
-            FitbitClient fitbitClient = new FitbitClient(appCredentials, FitbitController.currentAccessToken, true);
-            var profile = fitbitClient.GetUserProfileAsync(encodedUserId).Result;
+            FitbitClient fitbitClient = new(appCredentials, currentAccessToken, true);
+            UserProfile profile = fitbitClient.GetUserProfileAsync(encodedUserId).Result;
 
-            FitbitController.userProfile = profile;
+            userProfile = profile;
             return profile;
         }
 
@@ -106,44 +105,40 @@ namespace BBD.BodyMonitor.API.Controllers
         [Route("getsleepdata/{encodedUserId}/{date}")]
         public SleepSegment[] GetSleepData(string encodedUserId, DateTime date)
         {
-            if (FitbitController.currentAccessToken == null)
+            if (currentAccessToken == null)
             {
                 throw new Exception("Fitbit access token is null. You must first call 'fitbit/authenticate' and follow the URL that is returned.");
             }
 
-            if (userProfile == null)
-            {
-                userProfile = GetProfile(encodedUserId);
-            }
+            userProfile ??= GetProfile(encodedUserId);
 
-            FitbitClient fitbitClient = new FitbitClient(appCredentials, FitbitController.currentAccessToken, true);
-            var sleepData = fitbitClient.GetSleepDateRangeAsync(date.Date.AddDays(-1), date.Date.AddDays(2), encodedUserId).Result;
+            FitbitClient fitbitClient = new(appCredentials, currentAccessToken, true);
+            SleepDateRangeBase sleepData = fitbitClient.GetSleepDateRangeAsync(date.Date.AddDays(-1), date.Date.AddDays(2), encodedUserId).Result;
 
-            var sleepDataArray = sleepData.Sleep.SelectMany(s => s.Levels.Data).OrderBy(sld => sld.DateTime).ToArray();
+            LevelsData[] sleepDataArray = sleepData.Sleep.SelectMany(s => s.Levels.Data).OrderBy(sld => sld.DateTime).ToArray();
 
-            List<SleepSegment> result = new List<SleepSegment>();
+            List<SleepSegment> result = new();
             foreach (LevelsData ld in sleepDataArray)
             {
                 // times are LOCAL times
-                var ss = new SleepSegment()
+                SleepSegment ss = new()
                 {
                     Start = new DateTimeOffset(ld.DateTime, TimeSpan.FromMilliseconds(userProfile.OffsetFromUTCMillis)),
                     End = new DateTimeOffset(ld.DateTime, TimeSpan.FromMilliseconds(userProfile.OffsetFromUTCMillis)).AddSeconds(ld.Seconds),
+                    Level = ld.Level switch
+                    {
+                        "wake" => SleepLevel.Awake,
+                        "awake" => SleepLevel.Awake,
+                        "light" => SleepLevel.Light,
+                        "deep" => SleepLevel.Deep,
+                        "rem" => SleepLevel.REM,
+                        "restless" => SleepLevel.Restless,
+                        "asleep" => SleepLevel.Asleep,
+                        _ => SleepLevel.Unknown,
+                    }
                 };
 
-                ss.Level = ld.Level switch
-                {
-                    "wake" => SleepLevel.Awake,
-                    "awake" => SleepLevel.Awake,
-                    "light" => SleepLevel.Light,
-                    "deep" => SleepLevel.Deep,
-                    "rem" => SleepLevel.REM,
-                    "restless" => SleepLevel.Restless,
-                    "asleep" => SleepLevel.Asleep,
-                    _ => SleepLevel.Unknown,
-                };
-
-                if ((ss.Start.ToUniversalTime().Date != date.Date) && (ss.End.ToUniversalTime().Date != date.Date))
+                if (ss.Start.ToUniversalTime().Date != date.Date && ss.End.ToUniversalTime().Date != date.Date)
                 {
                     continue;
                 }
@@ -156,39 +151,37 @@ namespace BBD.BodyMonitor.API.Controllers
 
         [HttpGet]
         [Route("getheartrate/{encodedUserId}/{date}")]
+        [Obsolete]
         public HeartRateSegment[] GetHeartRate(string encodedUserId, DateTime date)
         {
-            if (FitbitController.currentAccessToken == null)
+            if (currentAccessToken == null)
             {
                 throw new Exception("Fitbit access token is null. You must first call 'fitbit/authenticate' and follow the URL that is returned.");
             }
 
-            if (userProfile == null)
-            {
-                userProfile = GetProfile(encodedUserId);
-            }
+            userProfile ??= GetProfile(encodedUserId);
 
-            FitbitClient fitbitClient = new FitbitClient(appCredentials, FitbitController.currentAccessToken, true);
-            var heartRateData = fitbitClient.GetHeartRateIntraday(date, HeartRateResolution.fiveSeconds, encodedUserId).Result;
-            var heartRateDataList = heartRateData.Dataset.ToList();
+            FitbitClient fitbitClient = new(appCredentials, currentAccessToken, true);
+            HeartActivitiesIntraday heartRateData = fitbitClient.GetHeartRateIntraday(date, HeartRateResolution.fiveSeconds, encodedUserId).Result;
+            List<DatasetInterval> heartRateDataList = heartRateData.Dataset.ToList();
             heartRateData = fitbitClient.GetHeartRateIntraday(date.AddDays(-1), HeartRateResolution.fiveSeconds, encodedUserId).Result;
             heartRateDataList.AddRange(heartRateData.Dataset);
             heartRateData = fitbitClient.GetHeartRateIntraday(date.AddDays(+1), HeartRateResolution.fiveSeconds, encodedUserId).Result;
             heartRateDataList.AddRange(heartRateData.Dataset);
 
-            
-            List<HeartRateSegment> result = new List<HeartRateSegment>();
+
+            List<HeartRateSegment> result = new();
             foreach (DatasetInterval di in heartRateDataList.OrderBy(hrd => hrd.TimeUtc).ToArray())
             {
                 // times are UTC times
-                var hrs = new HeartRateSegment()
+                HeartRateSegment hrs = new()
                 {
                     Start = new DateTimeOffset(di.TimeUtc, TimeSpan.Zero),
                     End = new DateTimeOffset(di.TimeUtc, TimeSpan.Zero).AddSeconds(5),
                     BeatsPerMinute = di.Value
                 };
 
-                if ((hrs.Start.ToUniversalTime().Date != date.Date) && (hrs.End.ToUniversalTime().Date != date.Date))
+                if (hrs.Start.ToUniversalTime().Date != date.Date && hrs.End.ToUniversalTime().Date != date.Date)
                 {
                     continue;
                 }
@@ -200,14 +193,15 @@ namespace BBD.BodyMonitor.API.Controllers
 
         [HttpGet]
         [Route("savefitbitdata/{subjectAlias}/{date?}")]
+        [Obsolete]
         public void SaveFitbitData(string subjectAlias, DateTime? date)
         {
-            if (FitbitController.currentAccessToken == null)
+            if (currentAccessToken == null)
             {
                 throw new Exception("Fitbit access token is null. You must first call 'fitbit/authenticate' and follow the URL that is returned.");
             }
 
-            var subject = _sessionManager.GetSubject(subjectAlias);
+            Subject? subject = _sessionManager.GetSubject(subjectAlias);
             if (subject == null)
             {
                 throw new Exception($"Subject '{subjectAlias}' not found.");
@@ -229,25 +223,25 @@ namespace BBD.BodyMonitor.API.Controllers
 
             for (int i = 0; i <= daysToGet; i++)
             {
-                var currentDate = date.Value.AddDays(i);
-                string fitbitSessionFilename = $"Subjects\\{subject.Alias}\\{subject.Alias}_{currentDate.ToString("yyyyMMdd_HHmmss")}__Fitbit.json";
+                DateTime currentDate = date.Value.AddDays(i);
+                string fitbitSessionFilename = $"Subjects\\{subject.Alias}\\{subject.Alias}_{currentDate:yyyyMMdd_HHmmss}__Fitbit.json";
                 string fullpathToSessionFile = Path.Combine(_sessionManager.MetadataDirectory, fitbitSessionFilename);
                 if (System.IO.File.Exists(fullpathToSessionFile))
                 {
                     if (currentDate.AddDays(3) < System.IO.File.GetLastWriteTimeUtc(fullpathToSessionFile))
                     {
                         // We have a supposedly complete file so we don't need to download the Fitbit data again.
-                        _logger.LogInformation($"Fitbit data on '{currentDate.ToString("yyyy-MM-dd")}' for subject '{subjectAlias}' seems to be up-to-date, so we don't download it.");
+                        _logger.LogInformation($"Fitbit data on '{currentDate:yyyy-MM-dd}' for subject '{subjectAlias}' seems to be up-to-date, so we don't download it.");
                         continue;
                     }
                 }
 
-                _logger.LogInformation($"Downloading Fitbit data on '{currentDate.ToString("yyyy-MM-dd")}' for subject '{subjectAlias}'.");
+                _logger.LogInformation($"Downloading Fitbit data on '{currentDate:yyyy-MM-dd}' for subject '{subjectAlias}'.");
 
-                var sleepData = GetSleepData(encodedUserId, currentDate);
-                var heartRateData = GetHeartRate(encodedUserId, currentDate);
+                SleepSegment[] sleepData = GetSleepData(encodedUserId, currentDate);
+                HeartRateSegment[] heartRateData = GetHeartRate(encodedUserId, currentDate);
 
-                var fitbitSession = new Session()
+                Session fitbitSession = new()
                 {
                     Version = 1,
                     SegmentedData = new SegmentedData()
