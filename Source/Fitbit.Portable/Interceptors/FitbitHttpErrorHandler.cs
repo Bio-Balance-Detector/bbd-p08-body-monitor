ea41914e28fff735d1808adceb7b25f5b5987781
Andras Fuchs <andras.fuchs@gmail.com>
2023-08-24 12:38:35 +0200

    Fix null return types

--- Source/Fitbit.Portable/Interceptors/FitbitHttpErrorHandler.cs
+++ Source/Fitbit.Portable/Interceptors/FitbitHttpErrorHandler.cs
@@ -1,22 +1,26 @@
-﻿using Fitbit.Models;
+﻿using Fitbit.Api.Portable;
+using Fitbit.Models;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Fitbit.Api.Portable.Interceptors
+namespace Fitbit.Portable.Interceptors
 {
     public class FitbitHttpErrorHandler : IFitbitInterceptor
     {
         public Task<HttpResponseMessage> InterceptRequest(HttpRequestMessage request, CancellationToken cancellationToken, FitbitClient invokingClient)
         {
-            return null;
+            return Task.FromResult<HttpResponseMessage>(null);
         }
 
         public async Task<HttpResponseMessage> InterceptResponse(Task<HttpResponseMessage> response, CancellationToken cancellationToken, FitbitClient invokingClient)
         {
-            if ((await response).IsSuccessStatusCode) return null;
+            if ((await response).IsSuccessStatusCode)
+            {
+                return null;
+            }
             else
             {
                 await GenerateFitbitRequestException(await response);
@@ -28,23 +32,22 @@ namespace Fitbit.Api.Portable.Interceptors
 
         private async Task GenerateFitbitRequestException(HttpResponseMessage response)
         {
-            List<ApiError> errors = null;
-
+            List<ApiError> errors;
             try
             {
                 // assumption is error response from fitbit in the 4xx range  
                 errors = new JsonDotNetSerializer().ParseErrors(await response.Content.ReadAsStringAsync());
             }
-            catch (ArgumentNullException emptyBodyException)
+            catch (ArgumentNullException)
             {
                 errors = new List<ApiError>() { { new ApiError() { ErrorType = "Fitbit.Net client library error", Message = "Error parsing content body. The content was empty" } } };
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 errors = new List<ApiError>() { { new ApiError() { ErrorType = "Fitbit.Net client library error", Message = "Unexpected error when deserializing the content of Fitbit's response." } } };
             }
 
-            var exception = new FitbitRequestException(response, errors);
+            FitbitRequestException exception = new FitbitRequestException(response, errors);
 
             throw exception;
         }
--- Source/Fitbit.Portable/OAuth2/OAuth2AutoRefreshInterceptor.cs
+++ Source/Fitbit.Portable/OAuth2/OAuth2AutoRefreshInterceptor.cs
@@ -1,10 +1,11 @@
-﻿using System.Diagnostics;
+﻿using Fitbit.Api.Portable;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Fitbit.Api.Portable.OAuth2
+namespace Fitbit.Portable.OAuth2
 {
     /// <summary>
     /// An Http interceptor that intercepts "stale token" responses and invokes the Token Manager of the FitbitClient to get a new token.
@@ -15,24 +16,24 @@ namespace Fitbit.Api.Portable.OAuth2
 
         public Task<HttpResponseMessage> InterceptRequest(HttpRequestMessage request, CancellationToken cancellationToken, FitbitClient Client)
         {
-            return null;
+            return Task.FromResult<HttpResponseMessage>(null);
         }
 
         public async Task<HttpResponseMessage> InterceptResponse(Task<HttpResponseMessage> response, CancellationToken cancellationToken, FitbitClient Client)
         {
             if (response.Result.StatusCode == System.Net.HttpStatusCode.Unauthorized)//Unauthorized, then there is a chance token is stale
             {
-                var responseBody = await response.Result.Content.ReadAsStringAsync();
+                string responseBody = await response.Result.Content.ReadAsStringAsync();
 
                 if (IsTokenStale(responseBody))
                 {
                     Debug.WriteLine("Stale token detected. Invoking registered tokenManager.RefreskToken to refresh it");
-                    await Client.RefreshOAuth2TokenAsync();
+                    _ = await Client.RefreshOAuth2TokenAsync();
 
                     //Only retry the first time.
                     if (!response.Result.RequestMessage.Headers.Contains(CUSTOM_HEADER))
                     {
-                        var clonedRequest = await response.Result.RequestMessage.CloneAsync();
+                        HttpRequestMessage clonedRequest = await response.Result.RequestMessage.CloneAsync();
                         clonedRequest.Headers.Add(CUSTOM_HEADER, CUSTOM_HEADER);
                         return await Client.HttpClient.SendAsync(clonedRequest, cancellationToken);
                     }
@@ -49,7 +50,7 @@ namespace Fitbit.Api.Portable.OAuth2
 
         private bool IsTokenStale(string responseBody)
         {
-            var errors = new JsonDotNetSerializer().ParseErrors(responseBody);
+            System.Collections.Generic.List<Models.ApiError> errors = new JsonDotNetSerializer().ParseErrors(responseBody);
             return errors.Any(error => error.ErrorType == "expired_token");
         }
     }
