2a117f0c2667cb45e2c218465c358f6b169f11de
Andras Fuchs <andras.fuchs@gmail.com>
2023-09-06 16:10:19 +0200

    Create IndicatorEvaluationResult.cs

---
+++ Source/BBD.BodyMonitor/Indicators/IndicatorEvaluationResult.cs
@@ -0,0 +1,23 @@
+﻿namespace BBD.BodyMonitor.Indicators
+{
+    public class IndicatorEvaluationResult
+    {
+        public long BlockIndex { get; set; }
+        public int IndicatorIndex { get; set; }
+        public required string IndicatorName { get; set; }
+        public required string Text { get; set; }
+        public float Value { get; set; }
+        public float PredictionScore { get; set; }
+        public bool IsTruePositive { get; set; }
+        public bool IsTrueNegative { get; set; }
+        public bool IsFalsePositive { get; set; }
+        public bool IsFalseNegative { get; set; }
+        public bool Negate { get; set; }
+        public required string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Text} {PredictionScore,7:+0.00;-0.00; 0.00}";
+        }
+    }
+}
